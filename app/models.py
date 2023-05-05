75a30b6d3daff67c882b2d2994e1f8e6fbdabb04
NiaPayton <shadeanpayton@gmail.com>
2023-05-05 07:26:57 -0500

    final touches

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from datetime import date
 
 class Movie(db.Model):
     
@@ -10,8 +11,11 @@ class Movie(db.Model):
     poster = db.Column(db.String(100))
     created_at = db.Column(db.Date)
     
-    def __init__(self, title, description, poster, created_at):
+    def __init__(self, title, description, poster):
         self.title = title
         self.description = description
         self.poster = poster
+
+        today = date.today()
+        created_at = today.strftime("%d/%m/%Y")
         self.created_at = created_at
